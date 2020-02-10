d1e99422ac1dee3fc82fcb2412b1bbdb0a359486
Harsh Shekhar <harshshekhar15@gmail.com>
2020-02-10 21:01:33 +0530

    Fix directory structure
    Signed-off-by: Harsh Shekhar <harshshekhar15@gmail.com>

--- oep-test-suite.sh
+++ oep-test-suite.sh
@@ -27,108 +27,108 @@ echo -e "------------------------------------------\n" >> result.txt
 
 # Run maya-io-server check
 echo -e "\n********** [ Running maya-io-server check ] **********\n";
-chmod 755 ./maya-io-server-check.sh
-./maya-io-server-check.sh > maya-io-server-check.log &
+chmod 755 ./basic-sanity-tests/maya-io-server-check.sh
+./basic-sanity-tests/maya-io-server-check.sh > ./logs/basic-sanity-tests/maya-io-server-check.log &
 
 # Run maya-ui check
 echo -e "\n************* [ Running maya-ui check ] **************\n";
-chmod 755 ./maya-ui-check.sh
-./maya-ui-check.sh > maya-ui-check.log &
+chmod 755 ./basic-sanity-tests/maya-ui-check.sh
+./basic-sanity-tests/maya-ui-check.sh > ./logs/basic-sanity-tests/maya-ui-check.log &
 
 # Run od-elasticsearch check
 echo -e "\n********* [ Running od-elasticsearch check ] *********\n";
-chmod 755 ./od-elasticsearch-check.sh
-./od-elasticsearch-check.sh > od-elasticsearch-check.log &
+chmod 755 ./basic-sanity-tests/od-elasticsearch-check.sh
+./basic-sanity-tests/od-elasticsearch-check.sh > ./logs/basic-sanity-tests/od-elasticsearch-check.log &
 
 # Run od-kibana check
 echo -e "\n************ [ Running od-kibana check ] *************\n";
-chmod 755 ./od-kibana-check.sh
-./od-kibana-check.sh > od-kibana-check.log &
+chmod 755 ./basic-sanity-tests/od-kibana-check.sh
+./basic-sanity-tests/od-kibana-check.sh > ./logs/basic-sanity-tests/od-kibana-check.log &
 
 # Run alertmanager check
 echo -e "\n*********** [ Running alertmanager check ] ***********\n";
-chmod 755 ./alertmanager-check.sh
-./alertmanager-check.sh > alertmanager-check.log &
+chmod 755 ./basic-sanity-tests/alertmanager-check.sh
+./basic-sanity-tests/alertmanager-check.sh > ./logs/basic-sanity-tests/alertmanager-check.log &
 
 # Run alertstore check
 echo -e "\n************ [ Running alertstore check ] ************\n";
-chmod 755 ./alertstore-check.sh
-./alertstore-check.sh > alertstore-check.log &
+chmod 755 ./basic-sanity-tests/alertstore-check.sh
+./basic-sanity-tests/alertstore-check.sh > ./logs/basic-sanity-tests/alertstore-check.log &
 
 # Run alertstore-tablemanager check
 echo -e "\n****** [ Running alertstore-tablemanager check ] *****\n";
-chmod 755 ./alertstore-tablemanager-check.sh
-./alertstore-tablemanager-check.sh > alertstore-tablemanager-check.log &
+chmod 755 ./basic-sanity-tests/alertstore-tablemanager-check.sh
+./basic-sanity-tests/alertstore-tablemanager-check.sh > ./logs/basic-sanity-tests/alertstore-tablemanager-check.log &
 
 # Run cassandra check
 echo -e "\n************ [ Running cassandra check ] *************\n";
-chmod 755 ./cassandra-check.sh
-./cassandra-check.sh > cassandra-check.log &
+chmod 755 ./basic-sanity-tests/cassandra-check.sh
+./basic-sanity-tests/cassandra-check.sh > ./logs/basic-sanity-tests/cassandra-check.log &
 
 # Run chat-server check
 echo -e "\n*********** [ Running chat-server check ] ************\n";
-chmod 755 ./chat-server-check.sh
-./chat-server-check.sh > chat-server-check.log &
+chmod 755 ./basic-sanity-tests/chat-server-check.sh
+./basic-sanity-tests/chat-server-check.sh > ./logs/basic-sanity-tests/chat-server-check.log &
 
 # Run cloud-agent check
 echo -e "\n*********** [ Running cloud-agent check ] ************\n";
-chmod 755 ./cloud-agent-check.sh
-./cloud-agent-check.sh > cloud-agent-check.log &
+chmod 755 ./basic-sanity-tests/cloud-agent-check.sh
+./basic-sanity-tests/cloud-agent-check.sh > ./logs/basic-sanity-tests/cloud-agent-check.log &
 
 # Run configs check
 echo -e "\n************* [ Running configs check ] **************\n";
-chmod 755 ./configs-check.sh
-./configs-check.sh > configs-check.log &
+chmod 755 ./basic-sanity-tests/configs-check.sh
+./basic-sanity-tests/configs-check.sh > ./logs/basic-sanity-tests/configs-check.log &
 
 # Run configs-db check
 echo -e "\n************ [ Running configs-db check ] ************\n";
-chmod 755 ./configs-db-check.sh
-./configs-db-check.sh > configs-db-check.log &
+chmod 755 ./basic-sanity-tests/configs-db-check.sh
+./basic-sanity-tests/configs-db-check.sh > ./logs/basic-sanity-tests/configs-db-check.log &
 
 # Run consul check
 echo -e "\n************** [ Running consul check ] **************\n";
-chmod 755 ./consul-check.sh
-./consul-check.sh > consul-check.log &
+chmod 755 ./basic-sanity-tests/consul-check.sh
+./basic-sanity-tests/consul-check.sh > ./logs/basic-sanity-tests/consul-check.log &
 
 # Run distributor check
 echo -e "\n************ [ Running distributor check ] ***********\n";
-chmod 755 ./distributor-check.sh
-./distributor-check.sh > distributor-check.log &
+chmod 755 ./basic-sanity-tests/distributor-check.sh
+./basic-sanity-tests/distributor-check.sh > ./logs/basic-sanity-tests/distributor-check.log &
 
 # Run ingester check
 echo -e "\n************* [ Running ingester check ] *************\n";
-chmod 755 ./ingester-check.sh
-./ingester-check.sh > ingester-check.log &
+chmod 755 ./basic-sanity-tests/ingester-check.sh
+./basic-sanity-tests/ingester-check.sh > ./logs/basic-sanity-tests/ingester-check.log &
 
 # Run ingress-nginx check
 echo -e "\n********** [ Running ingress-nginx check ] ***********\n";
-chmod 755 ./ingress-nginx-check.sh
-./ingress-nginx-check.sh > ingress-nginx-check.log &
+chmod 755 ./basic-sanity-tests/ingress-nginx-check.sh
+./basic-sanity-tests/ingress-nginx-check.sh > ./logs/basic-sanity-tests/ingress-nginx-check.log &
 
 # Run maya-grafana check
 echo -e "\n********** [ Running maya-grafana check ] ************\n";
-chmod 755 ./maya-grafana-check.sh
-./maya-grafana-check.sh > maya-grafana-check.log &
+chmod 755 ./basic-sanity-tests/maya-grafana-check.sh
+./basic-sanity-tests/maya-grafana-check.sh > ./logs/basic-sanity-tests/maya-grafana-check.log &
 
 # Run memcached check
 echo -e "\n************ [ Running memcached check ] *************\n";
-chmod 755 ./memcached-check.sh
-./memcached-check.sh > memcached-check.log &
+chmod 755 ./basic-sanity-tests/memcached-check.sh
+./basic-sanity-tests/memcached-check.sh > ./logs/basic-sanity-tests/memcached-check.log &
 
 # Run mysql check
 echo -e "\n************** [ Running mysql check ] ***************\n";
-chmod 755 ./mysql-check.sh
-./mysql-check.sh > mysql-check.log &
+chmod 755 ./basic-sanity-tests/mysql-check.sh
+./basic-sanity-tests/mysql-check.sh > ./logs/basic-sanity-tests/mysql-check.log &
 
 # Run querier check
 echo -e "\n************* [ Running querier check ] **************\n";
-chmod 755 ./querier-check.sh
-./querier-check.sh > querier-check.log &
+chmod 755 ./basic-sanity-tests/querier-check.sh
+./basic-sanity-tests/querier-check.sh > ./logs/basic-sanity-tests/querier-check.log &
 
 # Run ruler check
 echo -e "\n************** [ Running ruler check ] ***************\n";
-chmod 755 ./ruler-check.sh
-./ruler-check.sh > ruler-check.log &
+chmod 755 ./basic-sanity-tests/ruler-check.sh
+./basic-sanity-tests/ruler-check.sh > ./logs/basic-sanity-tests/ruler-check.log &
 
 wait
 
